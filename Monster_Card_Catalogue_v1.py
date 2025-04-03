bf1378b13b074847971a32804a33bf79d9b32273
iloveparkjisung <aryannemondeja@riccarton.school.nz>
2025-04-03 14:02:04 +1300

    I have made so when the user inputs the numbers it will go do the def functions and if it chooses 5 it quits the code and if something else is inputted and not part of the options it will tell the user to try again until they enter a valid choice.

--- Monster_Card_Catalogue_v1.py
+++ Monster_Card_Catalogue_v1.py
@@ -63,10 +63,30 @@ catalogue = {
 }
 
 def main_catalogue(): #this will be our main interface where we can navigate and locate what we want to see
-    print ("=== NEOZONE: THE Monster Game ===\n\n") #the welcoming message the user is receiving
+    print ("=== NEOZONE: THE Monster Game Catalogue ===\n") #the welcoming message the user is receiving
     while True:
-        print("Welcome to NEOZONE: THE Monster Game\n What would you like to do today?\n== Input the number to navigate ==") #this is the user's instruction
-        print("1 : View available NEOZONE monsters and stats!!\n2 : Look up a specific NEOZONE monsters\n3 : Make your own monster and stats\n4 : Delete a NEOZONE nonster\n5 : QUIT ") #the options the user can input
+        print("Welcome to NEOZONE: THE Monster Game Catalogue\n What would you like to do today?\n\n== Input the number to navigate ==") #this is the user's instruction
+        print("1 : View available NEOZONE monsters and stats!!\n2 : Look up a specific NEOZONE monsters\n3 : Make your own monster and stats\n4 : Delete a NEOZONE monster\n5 : QUIT ") #the options the user can input
         choice = input("Enter your choice: ")
+        if choice == '1':
+            view_monsters()
+        elif choice == '2':
+            search_catalogue()
+        elif choice == '3':
+            make_monster()
+        elif choice == '4':
+            delete_monster()
+        elif choice = '5':
+            print("Thank you for using NEOZONE: THE Monster Game Catalogue")
+            break
+        else:
+            print("That's not right. Please input the right numbers, Try Again.")
+        
 
-main_catalogue()
+def view_monsters():
+
+def search_catalogue():
+
+def make_monster():
+
+def delete_monster():
