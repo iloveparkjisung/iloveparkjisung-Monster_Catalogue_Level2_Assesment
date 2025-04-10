9f6e269c5a55419d8401dcce130c28f609f6e20c
iloveparkjisung <aryannemondeja@riccarton.school.nz>
2025-04-10 12:58:04 +1200

    ive copied and pasted the gui one so i can fix teh python one aswell

--- Monster_Card_Catalogue_v1.py
+++ Monster_Card_Catalogue_v1.py
@@ -116,16 +116,23 @@ def make_monster():#this function helps the user search up their desired monster
         return
     items = {} # we use an empty dictionary so we can put new things into it
     for stat_name in ["Strength", "Speed", "Stealth", "Cunning"]:
-        while True:
+                while True:
             try: 
-                stat_num = float(input(f"Choose a value for {stat_name}! (Please enter a number between 1 - 25) : "))
-                while stat_num > 25:
-                    print("Please enter a number between 1 - 25")
-                    break 
+                stat_num = eg.enterbox(f"Choose a value for {stat_name}! (Please enter a number between 1 - 25) : ", title = f"Set {stat_name}")
+                if stat_num is None:
+                    eg.msgbox("User entered a blank space. Monster creation canceled.\nReturning to the menu", "ERROR")
+                    return
+                try:
+                    stat_num = float(stat_num)
+                    items[stat_name] = stat_num
+                finally:
+                    if stat_num > 25:
+                        eg.msgbox("Please enter a number between 1 - 25!\nCanceling monster creation, returning to Main Menu.")
+                        return
                 items[stat_name] = stat_num
                 break
             except ValueError:
-                print("Invalid stat number. Please enter a numeric value")
+                eg.msgbox("Invalid stat number. Please enter a numeric value")
     catalogue [name] = items
     print(f"New NEOZONE monster '{name} added.")
 
